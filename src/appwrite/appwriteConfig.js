1f6ee1e4d7248a3aa4ac3b3b8d4c1ee089bb71b0
AgentRittik <109760373+AgentRittik@users.noreply.github.com>
2023-06-11 18:18:44 +0530

    fixing deploymrnt

--- src/appwrite/appwriteConfig.js
+++ src/appwrite/appwriteConfig.js
@@ -15,19 +15,13 @@
 // export const databases = new Databases(client, "647333601791c143edc3")
 
 import { Client, Account, Databases } from 'appwrite';
-import cors from 'cors';
 
 const client = new Client(); // Create a new Appwrite client
 
-// const requestOptions = {
-//   mode: 'no-cors', // Set the fetch mode to "no-cors" .setRequestOptions(requestOptions)
-// };
- 
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
-  
-  .setProject("647332cb9306f58de4a4"); // Set the Appwrite API endpoint, request options, and project ID
+  .setProject("64732c877001fa8c3d11"); // Set the Appwrite API endpoint, request options, and project ID
 
 export const account = new Account(client); // Create an instance of the Account class using the client
 
-export const databases = new Databases(client, "647333601791c143edc3"); // Create an instance of the Databases class using the client and database ID
+export const databases = new Databases(client, "64732d270cb87ea0eeb7"); // Create an instance of the Databases class using the client and database ID
